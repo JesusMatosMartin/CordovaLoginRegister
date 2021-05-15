037bc47a347cc4399cd94a851e921d6469523d06
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-15 16:18:02 +0200

    Login implemented

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,47 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+
+var formDiv = document.getElementById("form");
+
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var err = document.getElementById("err");
+var button = document.getElementById("loginButton");
+
+function loginProcess() {
+    if (username.value == "" || password.value == "") {
+        err.textContent = "Something's wrong. Please check the fields and try again.";
+        err.style.visibility = "visible";
+    } else if (localStorage.getItem(username.value) == null) {
+        err.textContent = "There's no account with this username registered.";
+        err.style.visibility = "visible";
+    } else {
+        err.style.visibility = "hidden";
+        if (checkUser()) {
+            alert("Logged in successfully");
+        } else {
+            err.textContent = "Password's wrong.";
+            err.style.visibility = "visible";
+        }
+    }
+}
+
+function checkUser() {
+    if (JSON.parse(localStorage.getItem(username.value)).password == CryptoJS.SHA256(password.value).toString()) {
+        return true;
+    }
+
+    return false;
+}
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+    button.onclick = loginProcess;
+    formDiv.onkeyup = function(event) {
+        if (event.key === "Enter") {
+            button.click();
+        }
+    }
 }
